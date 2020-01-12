c24af61cf25cf1195d39ee5e0023ba98cbde12fe
Juana Custodio <custodim.juana@gmail.com>
2020-01-12 18:45:54 -0500

    entity models

---
+++ CitasLimatambo/Models/EspecialidadPorSede.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CitasLimatambo.Models
+{
+    public class EspecialidadPorSede
+    {
+        public int ID { get; set; }
+        public int idSede { get; set; }
+        public int idEspecialidad { get; set; }
+
+        public virtual Sede sede { get; set; }
+        public virtual Especialidad especialidad { get; set; }
+    }
+}
---
+++ CitasLimatambo/Models/Sede.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CitasLimatambo.Models
+{
+    public class Sede
+    {
+        public int ID { get; set; }
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+    }
+}
